2b6df03a523567c9a90e0fe0caf43c5009a4a8d6
cguillen-gn <cguillen@geonet.es>
2025-12-01 13:14:27 +0100

    act forms

--- generador_formularios/gen_forms.py
+++ generador_formularios/gen_forms.py
@@ -30,7 +30,7 @@ MUNICIPIOS_TSV = "municipios.tsv"
 
 URL_ADJUNTOS = "https://script.google.com/macros/s/AKfycbwDOo2GzUU2P3pM5aZnSUhAirdViU5nrANrTeBAXti2gL71PDIzRbCh9JEljh-PAN5R/exec"
 URL_GOOGLE_FORMS = "https://docs.google.com/forms/d/e/1FAIpQLSc84PLY4O2wM9ek3v6L14DzZ8jcqDtFeKOK01i38s7ttPt0Ng/formResponse"
-URL_GENERAR_PDF = "https://script.google.com/macros/s/AKfycbymWMSBTaMFsJBstvpik6m3tSQqAfnZmJWO5wx9me05abNmvh3o-JXZYwn6Mrmat3Mj/exec";
+URL_GENERAR_PDF = "https://script.google.com/macros/s/AKfycbyyAdzIXagIitXq3PeT1Ht1yTPg0RWJa3SKUv37UvnkIUbdYVkhoZbDfrsU2f8vxL0t/exec";
 
 # ---------------- END CONFIG -----------------------------------
 
